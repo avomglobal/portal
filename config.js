02cf9741449a97c971097ac613ff59dc790ecede
avomglobal <avom.global@gmail.com>
2026-09-24 12:26:05 +0100

    Modify API_URL and LOGO_FILE_ID in config.js
    Updated API_URL and LOGO_FILE_ID in config.js.

--- config.js
+++ config.js
@@ -2,12 +2,12 @@
 // Change the text between the quotes, save the file, then refresh the page.
 window.ABBS_CONFIG = {
   // 1) Your Google Apps Script "Web app" URL (it ends in /exec).
-  //    Leave it empty to preview the page with demo data.
+  //    Leave it empty to preview the page with demo data. https://drive.google.com/file/d/1SZUlEdda779wazhhYKCn9NRHq6oWzYiZ/view?usp=sharing
   API_URL: 'https://script.google.com/macros/s/AKfycbwrkUyBS_l0zH4FTkLjTMV6EVzFYyU4MECawtx5bDKxglfT_TxGKF1jMLZ60ykCFWFQVQ/exec',
 
   // 2) Google Drive file ID of the school logo (the same one the PDF receipt uses).
   //    It is shown in the menu. This ID is not a secret.
-  LOGO_FILE_ID: '1_WOuCpKK8wK2xyQ7b4hb4G6A7uOV8TFB'
+  LOGO_FILE_ID: '1SZUlEdda779wazhhYKCn9NRHq6oWzYiZ'
 };
 
 // NEVER put the access key in this file. Staff type it on the sign-in screen. 
